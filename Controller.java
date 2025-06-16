c48af25fe8a06f279fc5c74e0c6705d21bdadf9f
k1rol <kirilka111007@gmail.com>
2025-06-16 20:00:44 +0200

    true second version of controller

--- Controller.java
+++ Controller.java
@@ -3,19 +3,32 @@ package com.example;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Controller {
     @FXML
-    private Button Button;
+    private TextField LOGINFIELD;
 
     @FXML
-    private Label Label;
+    private TextField PASSWORDFIELD;
 
     @FXML
-    private  void ButtonAction(){
-      Button.setOnAction(event -> {
-        Label.setText("CHUI");
-      }); 
+    private Button SUBMITBUTTON;
+    @FXML
+    private  void SUBMIT_BUTTON_ACTION(){
+     System.out.println(CheckingInstants());
     }
-    
+    private String  CheckingInstants(){
+        String Status;
+        if(LOGINFIELD.getText().isEmpty()){
+         return Status = "LOGIN FIELD IS EMPTHY";
+        }else if(PASSWORDFIELD.getText().isEmpty()){
+          return Status = "PASSWORD FIELD IS EMPTHY";
+        }else if(PASSWORDFIELD.getText().isEmpty()||LOGINFIELD.getText().isEmpty()){
+          return Status = "BOTH FIELDS ARE EMPTHY";
+        }else{
+          return Status = "ALL GOOD";
+        }
+        // return null;
+    }
 }
