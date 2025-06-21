562b530e43017c4e8c910ace78ef2792bbeb6937
k1rol <kirilka111007@gmail.com>
2025-06-21 20:14:39 +0200

    fixed error with database

--- DataBaseController.java
+++ DataBaseController.java
@@ -4,16 +4,18 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DataBaseController {
+  
     public void MainConnection(){
         try{
            String Url = "jdbc:mysql://localhost:3306/log_in_storeg";
            String Password = "1234567";
            String Name = "root";
+           Class.forName("com.mysql.cj.jdbc.Driver");
           //Connection connection = null;
           if(Name.isEmpty()||Password.isEmpty()||Url.isEmpty()){
             System.out.println("something went wrong while connecting database");   
           }else{
-            Connection connection = DriverManager.getConnection(Name,Password,Url);
+            Connection connection = DriverManager.getConnection(Url,Name,Password);
             System.out.println("DATA_BASE CONNECTED - All Good");
           }
         
